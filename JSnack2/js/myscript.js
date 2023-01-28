ff00b4b3d9fa6d6a0358976306017643aa698105
Rocca-Francesco <roccafrance98@yahoo.it>
2023-01-28 01:59:40 +0100

    JSnack2-final

--- JSnack2/js/myscript.js
+++ JSnack2/js/myscript.js
@@ -1,15 +1,40 @@
-// creo il mio array di numeri
-let numList = [];
-console.log(numList);
+const generaEl = document.getElementById("genera");
 
-// decido quanti numeri devo avere
-const numTot = Math.floor((Math.random() * 20) + 1);
-console.log(numTot);
-// genero dei numeri casuali e li aggiungo al mio array, finchè la quantità di numeri non è uguale a quella che devo avere, generata in precedenza
+generaEl.addEventListener(
+    "click",
+    function () {
+        // creo il mio array di numeri
+        let numList = [];
 
-for (i = 0; i < numTot; i++) {
-    let numRandom = Math.floor((Math.random() * 100) + 1);
-    numList.push(numRandom);
-    console.log(numList);
-}
+        // decido quanti numeri devo avere
+        const numTot = Math.floor((Math.random() * 30) + 1);
+
+        // genero dei numeri casuali e li aggiungo al mio array, finchè la quantità di numeri non è uguale a quella che devo avere, generata in precedenza
+        for (i = 0; i < numTot; i++) {
+            let numRandom = Math.floor((Math.random() * 100) + 1);
+            numList.push(numRandom);
+        };
+
+        // dichiaro altri due array per i numeri pari e dispari
+        let dispariList = [];
+        let pariList = [];
+
+        // per ogni numero generato, scelgo se è pari o dispari e lo divido negli array rispettivi
+        for (i = 0; i < numList.length; i++) {
+            if (numList[i] % 2 == 0) {
+                pariList.push(numList[i]);
+            } else {
+                dispariList.push(numList[i]);
+            }
+        };
+
+        // scrivo la mia lista generata random e le relative suddivisioni
+        document.getElementById("random").innerHTML = "Questi sono i numeri generati randomicamente: " + numList + ".";
+        document.getElementById("dispari").innerHTML = "Questi sono i numeri dispari: " + dispariList + ".";
+        document.getElementById("pari").innerHTML = "Questi sono i numeri pari: " + pariList + ".";
+    }
+);
+
+
+
 
